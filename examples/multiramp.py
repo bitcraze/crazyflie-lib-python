6f5db702b3fb510945af2721f758a9b771681cef
Arnaud Taffanel <arnaud@bitcraze.se>
2016-11-29 11:42:55 +0100

    Fix single-quoted strings in multiramp

--- examples/multiramp.py
+++ examples/multiramp.py
@@ -53,7 +53,7 @@ class MotorRampExample:
 
         self._cf.open_link(link_uri)
 
-        print('Connecting to %s' % link_uri)
+        print("Connecting to %s" % link_uri)
 
     def _connected(self, link_uri):
         """ This callback is called form the Crazyflie API when a Crazyflie
@@ -66,16 +66,16 @@ class MotorRampExample:
     def _connection_failed(self, link_uri, msg):
         """Callback when connection initial connection fails (i.e no Crazyflie
         at the specified address)"""
-        print('Connection to %s failed: %s' % (link_uri, msg))
+        print("Connection to %s failed: %s" % (link_uri, msg))
 
     def _connection_lost(self, link_uri, msg):
         """Callback when disconnected after a connection has been made (i.e
         Crazyflie moves out of range)"""
-        print('Connection to %s lost: %s' % (link_uri, msg))
+        print("Connection to %s lost: %s" % (link_uri, msg))
 
     def _disconnected(self, link_uri):
         """Callback when the Crazyflie is disconnected (called in all cases)"""
-        print('Disconnected from %s' % link_uri)
+        print("Disconnected from %s" % link_uri)
 
     def _ramp_motors(self):
         thrust_mult = 1
@@ -101,7 +101,7 @@ class MotorRampExample:
         self._cf.close_link()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Initialize the low-level drivers (don't list the debug drivers)
     cflib.crtp.init_drivers(enable_debug_driver=False)
     # Connect the two Crazyflies and ramps them up-down

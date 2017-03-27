fe61de6d87bbe23185b5b187097f277a14b0ed36
Kristoffer Richardsson <kristoffer@bitcraze.io>
2017-03-27 16:12:32 +0200

    Added printing of the estimated position in the autonomousSequence example. Disabled by default. Closes #43

--- examples/autonomousSequence.py
+++ examples/autonomousSequence.py
@@ -105,6 +105,24 @@ def reset_estimator(scf):
     wait_for_position_estimator(cf)
 
 
+def position_callback(timestamp, data, logconf):
+    x = data['kalman.stateX']
+    y = data['kalman.stateY']
+    z = data['kalman.stateZ']
+    print('pos: ({}, {}, {})'.format(x, y, z))
+
+
+def start_position_printing(scf):
+    log_conf = LogConfig(name='Position', period_in_ms=500)
+    log_conf.add_variable('kalman.stateX', 'float')
+    log_conf.add_variable('kalman.stateY', 'float')
+    log_conf.add_variable('kalman.stateZ', 'float')
+
+    scf.cf.log.add_config(log_conf)
+    log_conf.data_received_cb.add_callback(position_callback)
+    log_conf.start()
+
+
 def run_sequence(scf, sequence):
     cf = scf.cf
 
@@ -129,4 +147,5 @@ if __name__ == '__main__':
 
     with SyncCrazyflie(uri) as scf:
         reset_estimator(scf)
+        # start_position_printing(scf)
         run_sequence(scf, sequence)

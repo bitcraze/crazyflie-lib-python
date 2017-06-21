a697c0d4c948517063065b30180a04c1258e389e
Arnaud Taffanel <arnaud@bitcraze.se>
2017-06-21 15:59:14 +0200

    #50: Add example using the new flow deck

---
+++ examples/flowsequenceSync.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+#
+#     ||          ____  _ __
+#  +------+      / __ )(_) /_______________ _____  ___
+#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#
+#  Copyright (C) 2016 Bitcraze AB
+#
+#  Crazyflie Nano Quadcopter Client
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA  02110-1301, USA.
+"""
+Simple example that connects to the crazyflie at `URI` and runs a 8 sequence.
+This script requires some kind of location system, it has been tested with
+(and designed for) the flow deck.
+
+Change the URI variable to your Crazyflie configuration.
+"""
+import logging
+import time
+
+import cflib.crtp
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+
+URI = "radio://0/80/250K"
+
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+
+
+if __name__ == '__main__':
+    # Initialize the low-level drivers (don't list the debug drivers)
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+
+    with SyncCrazyflie(URI) as scf:
+        cf = scf.cf
+
+        cf.param.set_value('kalman.resetEstimation', '1')
+        time.sleep(0.1)
+        cf.param.set_value('kalman.resetEstimation', '0')
+        time.sleep(2)
+
+        for _ in range(20):
+            cf.commander.send_hover_setpoint(0, 0, 0, 0.2)
+            time.sleep(0.1)
+
+        for _ in range(30):
+            cf.commander.send_hover_setpoint(0, 0, 0, 0.4)
+            time.sleep(0.1)
+
+        for _ in range(100):
+            cf.commander.send_hover_setpoint(0.5, 0, 36, 0.4)
+            time.sleep(0.1)
+
+        for _ in range(100):
+            cf.commander.send_hover_setpoint(0.5, 0, -36, 0.4)
+            time.sleep(0.1)
+
+        for _ in range(30):
+            cf.commander.send_hover_setpoint(0, 0, 0, 0.4)
+            time.sleep(0.1)
+
+        for _ in range(30):
+            cf.commander.send_hover_setpoint(0, 0, 0, 0.2)
+            time.sleep(0.1)
+
+        cf.commander.send_stop_setpoint()

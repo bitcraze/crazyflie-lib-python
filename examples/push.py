b3656e35d6d0f5987abb5dc458abf2f19b1855a8
Kristoffer Richardsson <kristoffer@bitcraze.io>
2018-01-31 15:19:54 +0100

    #71 Simplified the push example script

--- examples/push.py
+++ examples/push.py
@@ -26,14 +26,14 @@
 #  MA  02110-1301, USA.
 """
 Example scipts that allows a user to "push" the Crazyflie 2.0 around
-using their hands while it's hovering.
+using your hands while it's hovering.
 
-This examples uses the Flow and Multi-ranger decks to read out distances
+This examples uses the Flow and Multi-ranger decks to measure distances
 in all directions and tries to keep away from anything that comes closer
-than 0.2m by setting a velocity in the oposite direction.
+than 0.2m by setting a velocity in the opposite direction.
 
-The demo is ended by either pressing Ctrl-C or by "pushing" the Crazyflie 2.0
-down so it get's closer than 0.1m to the surface it's hoving above.
+The demo is ended by either pressing Ctrl-C or by holding your hand above the
+Crazyflie.
 
 For the example to run the following hardware is needed:
  * Crazyflie 2.0
@@ -46,11 +46,12 @@ import sys
 import time
 
 import cflib.crtp
-from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
-from cflib.crazyflie.syncLogger import SyncLogger
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils.multi_ranger import MultiRanger
 
-URI = 'radio://0/80/250K'
+URI = 'radio://0/80/2M'
 
 if len(sys.argv) > 1:
     URI = sys.argv[1]
@@ -59,88 +60,46 @@ if len(sys.argv) > 1:
 logging.basicConfig(level=logging.ERROR)
 
 
+def is_close(range):
+    MIN_DISTANCE = 0.2  # m
+
+    if range is None:
+        return False
+    else:
+        return range < MIN_DISTANCE
+
+
 if __name__ == '__main__':
     # Initialize the low-level drivers (don't list the debug drivers)
     cflib.crtp.init_drivers(enable_debug_driver=False)
 
-    with SyncCrazyflie(URI) as scf:
-        cf = scf.cf
+    cf = Crazyflie(rw_cache='./cache')
+    with SyncCrazyflie(URI, cf=cf) as scf:
+        with MotionCommander(scf) as motion_commander:
+            with MultiRanger(scf) as multi_ranger:
+                keep_flying = True
 
-        # Reset Kalman filter on startup
-        cf.param.set_value('kalman.resetEstimation', '1')
-        time.sleep(0.1)
-        cf.param.set_value('kalman.resetEstimation', '0')
-        time.sleep(2)
+                while keep_flying:
+                    VELOCITY = 0.5
+                    velocity_x = 0.0
+                    velocity_y = 0.0
 
-        # Start logblock with distances in all directions at 100Hz
-        lg_oa = LogConfig(name='OA', period_in_ms=10)
-        lg_oa.add_variable('oa.front')
-        lg_oa.add_variable('oa.back')
-        lg_oa.add_variable('oa.up')
-        lg_oa.add_variable('oa.left')
-        lg_oa.add_variable('oa.right')
-        lg_oa.add_variable('range.zrange')
+                    if is_close(multi_ranger.front):
+                        velocity_x -= VELOCITY
+                    if is_close(multi_ranger.back):
+                        velocity_x += VELOCITY
 
-        # The max ramp up velocity
-        max_vel = 1.0  # m/s
-        # The detection radius (front/back/left/right/up)
-        radius = 0.2  # m
-        # The hover height (down)
-        height_sp = 0.2  # m
-        # Threashold for switching off the Crazyflie when pushed down
-        landing_height = 0.1  # m
-        # Factor for raming up when keeping radius
-        factor = max_vel / radius
-        # Should we stop flying or not
-        stop = False
+                    if is_close(multi_ranger.left):
+                        velocity_y -= VELOCITY
+                    if is_close(multi_ranger.right):
+                        velocity_y += VELOCITY
 
-        with SyncLogger(scf, lg_oa) as logger:
-            # Running at 100Hz
-            for log_entry in logger:
-                # Various vabiables that are sent together with the
-                # log data but not used in this example
-                timestamp = log_entry[0]
-                data = log_entry[1]
-                logconf_name = log_entry[2]
+                    if is_close(multi_ranger.up):
+                        keep_flying = False
 
-                # Distances are reported in mm, convert into m
-                fw_distance = data['oa.front'] / 1000.0
-                bk_distance = data['oa.back'] / 1000.0
-                r_distance = data['oa.right'] / 1000.0
-                l_distance = data['oa.left'] / 1000.0
-                up_distance = data['oa.up'] / 1000.0
-                down_distance = data['range.zrange'] / 1000.0
+                    motion_commander.start_linear_motion(
+                        velocity_x, velocity_y, 0)
 
-                # Calculate the distance inside the radius
-                #   X    <----)
-                fw_o = radius - min(fw_distance, radius)
-                bk_o = radius - min(bk_distance, radius)
-                l_o = radius - min(l_distance, radius)
-                r_o = radius - min(r_distance, radius)
-                u_o = radius - min(up_distance, radius)
+                    time.sleep(0.1)
 
-                # Weigh together oposite sides. I.e if you are getting close
-                # on both sides they are both taken into account.
-                fw_comp = (-1) * fw_o * factor
-                bk_comp = bk_o * factor
-                fw_vel = fw_comp + bk_comp
-
-                l_comp = (-1) * l_o * factor
-                r_comp = r_o * factor
-                side_vel = l_comp + r_comp
-
-                height = height_sp - u_o
-
-                # If we should still be flying send the hover set-points
-                # Note that these are in meters and meters/second
-                if not stop:
-                    cf.commander.send_hover_setpoint(
-                        fw_vel, side_vel, 0, height)
-
-                # If we're pushing the Crazyflie towards the ground it will
-                # shut down if you get close enough
-                if down_distance < landing_height and height < height_sp:
-                    stop = True
-                    cf.commander.send_stop_setpoint()
-                    print('Demo terminated!')
-                    sys.exit(1)
+            print('Demo terminated!')

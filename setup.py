19763b79847b4497221e1d2bcd128701862d0646
Arnaud Taffanel <arnaud@bitcraze.io>
2018-10-16 15:52:08 +0200

    Release 0.1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='cflib',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(exclude=['examples', 'tests']),
 
     description='Crazyflie python driver',

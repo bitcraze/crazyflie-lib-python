8e67108991ad557b38cac47d052267514aa97371
Kristoffer Richardsson <kristoffer@bitcraze.io>
2019-04-30 16:27:32 +0200

    #117 Added memory mapping for Lighthouse base station geometry data.

--- cflib/crazyflie/mem.py
+++ cflib/crazyflie/mem.py
@@ -74,6 +74,7 @@ class MemoryElement(object):
     TYPE_LOCO = 0x11
     TYPE_TRAJ = 0x12
     TYPE_LOCO2 = 0x13
+    TYPE_LH = 0x14
 
     def __init__(self, id, type, size, mem_handler):
         """Initialize the element with default values"""
@@ -97,6 +98,8 @@ class MemoryElement(object):
             return 'Trajectory'
         if t == MemoryElement.TYPE_LOCO2:
             return 'Loco Positioning 2'
+        if t == MemoryElement.TYPE_LH:
+            return 'Lighthouse positioning'
         return 'Unknown'
 
     def new_data(self, mem, addr, data):
@@ -711,6 +714,113 @@ class TrajectoryMemory(MemoryElement):
         self._write_finished_cb = None
 
 
+class LighthouseBsGeometry:
+    """Container for geometry data of one Lighthouse base station"""
+
+    SIZE_FLOAT = 4
+    SIZE_VECTOR = 3 * SIZE_FLOAT
+    SIZE_GEOMETRY = (1 + 3) * SIZE_VECTOR
+    SIZE_DATA = 2 * SIZE_GEOMETRY
+
+    def __init__(self):
+        self.origin = [0.0, 0.0, 0.0]
+        self.rotation_matrix = [
+            [0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0],
+        ]
+
+    def set_from_mem_data(self, data):
+        self.origin = self._read_vector(
+            data[0 * self.SIZE_VECTOR:1 * self.SIZE_VECTOR])
+        self.rotation_matrix = [
+            self._read_vector(data[1 * self.SIZE_VECTOR:2 * self.SIZE_VECTOR]),
+            self._read_vector(data[2 * self.SIZE_VECTOR:3 * self.SIZE_VECTOR]),
+            self._read_vector(data[3 * self.SIZE_VECTOR:4 * self.SIZE_VECTOR]),
+        ]
+
+    def add_mem_data(self, data):
+        self._add_vector(data, self.origin)
+        self._add_vector(data, self.rotation_matrix[0])
+        self._add_vector(data, self.rotation_matrix[1])
+        self._add_vector(data, self.rotation_matrix[2])
+
+    def _add_vector(self, data, vector):
+        data += struct.pack('<fff', vector[0], vector[1], vector[2])
+
+    def _read_vector(self, data):
+        x, y, z = struct.unpack('<fff', data)
+        return [x, y, z]
+
+    def dump(self):
+        print('origin:', self.origin)
+        print('rotation matrix: ', self.rotation_matrix)
+
+
+class LighthouseMemory(MemoryElement):
+    """
+    Memory interface for lighthouse configuration data
+    """
+
+    def __init__(self, id, type, size, mem_handler):
+        """Initialize Lighthouse memory"""
+        super(LighthouseMemory, self).__init__(id=id, type=type, size=size,
+                                               mem_handler=mem_handler)
+
+        self._update_finished_cb = None
+        self._write_finished_cb = None
+
+        # Geometry data for two base stations
+        self.geometry_data = [
+            LighthouseBsGeometry(),
+            LighthouseBsGeometry(),
+        ]
+
+    def new_data(self, mem, addr, data):
+        """Callback for when new memory data has been fetched"""
+        if mem.id == self.id:
+            if addr == 0:
+                self.geometry_data[0].set_from_mem_data(
+                    data[0:LighthouseBsGeometry.SIZE_GEOMETRY])
+                self.geometry_data[1].set_from_mem_data(
+                    data[LighthouseBsGeometry.SIZE_GEOMETRY:])
+
+                if self._update_finished_cb:
+                    self._update_finished_cb(self)
+                    self._update_finished_cb = None
+
+    def update(self, update_finished_cb):
+        """Request an update of the memory content"""
+        if not self._update_finished_cb:
+            self._update_finished_cb = update_finished_cb
+            logger.debug('Updating content of memory {}'.format(self.id))
+            self.mem_handler.read(self, 0, LighthouseBsGeometry.SIZE_DATA)
+
+    def write_data(self, write_finished_cb):
+        """Write geometry data to the Crazyflie"""
+        self._write_finished_cb = write_finished_cb
+        data = bytearray()
+
+        for bs in self.geometry_data:
+            bs.add_mem_data(data)
+
+        self.mem_handler.write(self, 0x00, data, flush_queue=True)
+
+    def write_done(self, mem, addr):
+        if self._write_finished_cb and mem.id == self.id:
+            logger.debug('Write of geometry data done')
+            self._write_finished_cb(self, addr)
+            self._write_finished_cb = None
+
+    def disconnect(self):
+        self._update_finished_cb = None
+        self._write_finished_cb = None
+
+    def dump(self):
+        for data in self.geometry_data:
+            data.dump()
+
+
 class _ReadRequest:
     """
     Class used to handle memory reads that will split up the read in multiple
@@ -1084,6 +1194,12 @@ class Memory():
                                           size=mem_size, mem_handler=self)
                         logger.debug(mem)
                         self.mem_read_cb.add_callback(mem.new_data)
+                    elif mem_type == MemoryElement.TYPE_LH:
+                        mem = LighthouseMemory(id=mem_id, type=mem_type,
+                                               size=mem_size, mem_handler=self)
+                        logger.debug(mem)
+                        self.mem_read_cb.add_callback(mem.new_data)
+                        self.mem_write_cb.add_callback(mem.write_done)
                     else:
                         mem = MemoryElement(id=mem_id, type=mem_type,
                                             size=mem_size, mem_handler=self)
---
+++ examples/lighthouse/read-geometry-mem.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+#
+#     ||          ____  _ __
+#  +------+      / __ )(_) /_______________ _____  ___
+#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#
+#  Copyright (C) 2019 Bitcraze AB
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA  02110-1301, USA.
+"""
+Example of how to read the Lighthouse base station geometry memory from a
+Crazyflie
+"""
+import logging
+import time
+
+import cflib.crtp  # noqa
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.mem import MemoryElement
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+# Only output errors from the logging framework
+
+logging.basicConfig(level=logging.ERROR)
+
+
+class ReadMem:
+    def __init__(self, uri):
+        self.got_data = False
+
+        with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+            mems = scf.cf.mem.get_mems(MemoryElement.TYPE_LH)
+
+            count = len(mems)
+            if count != 1:
+                raise Exception('Unexpected nr of memories found:', count)
+
+            print('Rquesting data')
+            mems[0].update(self._data_updated)
+
+            while not self.got_data:
+                time.sleep(1)
+
+    def _data_updated(self, mem):
+        mem.dump()
+        self.got_data = True
+
+
+if __name__ == '__main__':
+    # Initialize the low-level drivers (don't list the debug drivers)
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+
+    # Scan for Crazyflies and use the first one found
+    print('Scanning interfaces for Crazyflies...')
+    available = cflib.crtp.scan_interfaces()
+
+    print('Crazyflies found:')
+    for i in available:
+        print(i[0])
+
+    if len(available) > 0:
+        uri = available[0][0]
+        ReadMem(uri)
+
+    else:
+        print('No Crazyflies found, cannot run example')

40e6f4b884b5fc971db84d14e1a8f7fa7e9e2de4
Kristoffer Richardsson <kristoffer@bitcraze.io>
2019-05-02 10:42:34 +0200

    Added script for writing LH geometry data. Closes #117

--- examples/lighthouse/write-geometry-mem.py
+++ examples/lighthouse/write-geometry-mem.py
@@ -22,7 +22,7 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA  02110-1301, USA.
 """
-Example of how to read the Lighthouse base station geometry memory from a
+Example of how to write to the Lighthouse base station geometry memory in a
 Crazyflie
 """
 import logging
@@ -30,6 +30,7 @@ import time
 
 import cflib.crtp  # noqa
 from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.mem import LighthouseBsGeometry
 from cflib.crazyflie.mem import MemoryElement
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 # Only output errors from the logging framework
@@ -37,9 +38,9 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 logging.basicConfig(level=logging.ERROR)
 
 
-class ReadMem:
-    def __init__(self, uri):
-        self.got_data = False
+class WriteMem:
+    def __init__(self, uri, bs1, bs2):
+        self.data_written = False
 
         with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
             mems = scf.cf.mem.get_mems(MemoryElement.TYPE_LH)
@@ -48,15 +49,17 @@ class ReadMem:
             if count != 1:
                 raise Exception('Unexpected nr of memories found:', count)
 
-            print('Rquesting data')
-            mems[0].update(self._data_updated)
+            mems[0].geometry_data = [bs1, bs2]
 
-            while not self.got_data:
+            print('Writing data')
+            mems[0].write_data(self._data_written)
+
+            while not self.data_written:
                 time.sleep(1)
 
-    def _data_updated(self, mem):
-        mem.dump()
-        self.got_data = True
+    def _data_written(self, mem, addr):
+        self.data_written = True
+        print('Data written')
 
 
 if __name__ == '__main__':
@@ -66,4 +69,20 @@ if __name__ == '__main__':
     # Initialize the low-level drivers (don't list the debug drivers)
     cflib.crtp.init_drivers(enable_debug_driver=False)
 
-    ReadMem(uri)
+    bs1 = LighthouseBsGeometry()
+    bs1.origin = [1.0, 2.0, 3.0]
+    bs1.rotation_matrix = [
+        [4.0, 5.0, 6.0],
+        [7.0, 8.0, 9.0],
+        [10.0, 11.0, 12.0],
+    ]
+
+    bs2 = LighthouseBsGeometry()
+    bs2.origin = [21.0, 22.0, 23.0]
+    bs2.rotation_matrix = [
+        [24.0, 25.0, 26.0],
+        [27.0, 28.0, 29.0],
+        [30.0, 31.0, 32.0],
+    ]
+
+    WriteMem(uri, bs1, bs2)

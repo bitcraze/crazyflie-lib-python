b97913762f6b5dae33c4d1767827d524f21ac1b7
Arnaud Taffanel <arnaud@bitcraze.io>
2021-01-26 10:30:11 +0100

    Update setup.py version to 0.13

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='cflib',
-    version='0.1.12.1',
+    version='0.1.13',
     packages=find_packages(exclude=['examples', 'tests']),
 
     description='Crazyflie python driver',

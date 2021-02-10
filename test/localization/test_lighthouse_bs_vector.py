680e7241e536568b2481fcbeda0866e23fa966f8
Kristoffer Richardsson <kristoffer@bitcraze.io>
2021-02-10 14:50:07 +0100

    #192 Fixed imports

--- test/localization/test_lighthouse_bs_vector.py
+++ test/localization/test_lighthouse_bs_vector.py
@@ -19,9 +19,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 import unittest
-import math
+
 import numpy as np
 
 from cflib.localization.lighthouse_bs_vector import LighthouseBsVector

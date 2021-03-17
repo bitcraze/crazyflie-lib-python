366593451c55f4a6380535d0797737a4dafe8543
Arnaud Taffanel <arnaud@bitcraze.io>
2021-03-17 14:13:46 +0100

    Update version to 0.1.14

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='cflib',
-    version='0.1.13.1',
+    version='0.1.14',
     packages=find_packages(exclude=['examples', 'tests']),
 
     description='Crazyflie python driver',

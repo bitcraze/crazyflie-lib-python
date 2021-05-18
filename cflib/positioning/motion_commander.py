313a344072267b0a2396888a9c066cbb403d334a
knmcguire <kimberly@bitcraze.io>
2021-05-18 10:58:12 +0200

    Made possible to give a velocity command and rate input at the same time for motion commander

--- cflib/positioning/motion_commander.py
+++ cflib/positioning/motion_commander.py
@@ -399,6 +399,23 @@ class MotionCommander:
         self._set_vel_setpoint(
             velocity_x_m, velocity_y_m, velocity_z_m, 0.0)
 
+    def start_motion(self, velocity_x_m, velocity_y_m, velocity_z_m, rate):
+        """
+        Start a velocity motion with yaw rate input. This function returns immediately.
+
+        positive X is forward
+        positive Y is left
+        positive Z is up
+
+        :param velocity_x_m: The velocity along the X-axis (meters/second)
+        :param velocity_y_m: The velocity along the Y-axis (meters/second)
+        :param velocity_z_m: The velocity along the Z-axis (meters/second)
+        :param rate: The angular rate (degrees/second)
+        :return:
+        """
+        self._set_vel_setpoint(
+            velocity_x_m, velocity_y_m, velocity_z_m, rate)
+
     def _set_vel_setpoint(self, velocity_x, velocity_y, velocity_z, rate_yaw):
         if not self._is_flying:
             raise Exception('Can not move on the ground. Take off first!')

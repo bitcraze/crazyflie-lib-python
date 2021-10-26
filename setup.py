2f411e6af37705c030d0a61a78877ffbfd4c99bf
Arnaud Taffanel <arnaud@bitcraze.io>
2021-10-26 08:52:34 +0200

    Relax opencv dependency version, fixes py3.10 test
    Fixes Install fails on Python 3.10
    Fixes #267

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
 
     install_requires=[
         'pyusb>=1.0.0b2',
-        'opencv-python-headless==4.5.1.48'
+        'opencv-python-headless~=4.5.1'
     ],
 
     # $ pip install -e .[dev]

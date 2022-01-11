5b64572a1b5c52dbe0b500536c5a77807ac3e6e6
Jonas Danielsson <jonas@bitcraze.io>
2022-01-11 06:44:02 +0100

    Update GNU copying permission statement
    We do not want to keep drack of the Free Software Foundation office.
    Instead we use the recommened lines from https://www.gnu.org/licenses/gpl-howto.en.html

--- examples/multiranger/multiranger_push.py
+++ examples/multiranger/multiranger_push.py
@@ -20,10 +20,8 @@
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA  02110-1301, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 Example scripts that allows a user to "push" the Crazyflie 2.0 around
 using your hands while it's hovering.

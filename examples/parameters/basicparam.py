3e376f2cb8fd7131253d91dd5cc31d02b4e31a9e
Kristoffer Richardsson <kristoffer@bitcraze.io>
2022-06-28 11:39:12 +0200

    Update basicparam example

--- examples/parameters/basicparam.py
+++ examples/parameters/basicparam.py
@@ -34,7 +34,7 @@ import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.utils import uri_helper
 
-address = uri_helper.address_from_env(default=0xE7E7E7E7E7)
+address = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
 
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
@@ -53,6 +53,7 @@ class ParamExample:
 
         # Connect some callbacks from the Crazyflie API
         self._cf.connected.add_callback(self._connected)
+        self._cf.fully_connected.add_callback(self._fully_connected)
         self._cf.disconnected.add_callback(self._disconnected)
         self._cf.connection_failed.add_callback(self._connection_failed)
         self._cf.connection_lost.add_callback(self._connection_lost)
@@ -72,7 +73,7 @@ class ParamExample:
 
     def _connected(self, link_uri):
         """ This callback is called form the Crazyflie API when a Crazyflie
-        has been connected and the TOCs have been downloaded."""
+        has been connected and the TOCs have been downloaded. Parameter values are not downloaded yet."""
         print('Connected to %s' % link_uri)
 
         # Print the param TOC
@@ -84,14 +85,28 @@ class ParamExample:
                 self._param_check_list.append('{0}.{1}'.format(group, param))
             self._param_groups.append('{}'.format(group))
             # For every group, register the callback
-            self._cf.param.add_update_callback(group=group, name=None,
-                                               cb=self._param_callback)
+            self._cf.param.add_update_callback(group=group, name=None, cb=self._param_callback)
 
         # You can also register a callback for a specific group.name combo
         self._cf.param.add_update_callback(group='cpu', name='flash',
                                            cb=self._cpu_flash_callback)
 
         print('')
+
+    def _fully_connected(self, link_uri):
+        """This callback is called when the Crazyflie has been connected and all parameters have been
+        downloaded. It is now OK to set and get parameters."""
+        print(f'Parameters downloaded to {link_uri}')
+
+        # We can get a parameter value directly without using a callback
+        value = self._cf.param.get_value('pid_attitude.pitch_kd')
+        print(f'Value read with get() is {value}')
+
+        # When a parameter is set, the callback is called with the new value
+        self._cf.param.add_update_callback(group='pid_attitude', name='pitch_kd', cb=self._a_pitch_kd_callback)
+        # When setting a value the parameter is automatically read back
+        # and the registered callbacks will get the updated value
+        self._cf.param.set_value('pid_attitude.pitch_kd', 0.1234)
 
     def _cpu_flash_callback(self, name, value):
         """Specific callback for the cpu.flash parameter"""
@@ -101,35 +116,20 @@ class ParamExample:
         """Generic callback registered for all the groups"""
         print('{0}: {1}'.format(name, value))
 
-        # Remove each parameter from the list and close the link when
-        # all are fetched
+        # Remove each parameter from the list when fetched
         self._param_check_list.remove(name)
         if len(self._param_check_list) == 0:
             print('Have fetched all parameter values.')
 
-            # First remove all the group callbacks
+            # Remove all the group callbacks
             for g in self._param_groups:
-                self._cf.param.remove_update_callback(group=g,
-                                                      cb=self._param_callback)
-
-            # Create a new random value [0.00,1.00] for pid_attitude.pitch_kd
-            # and set it
-            pkd = random.random()
-            print('')
-            print('Write: pid_attitude.pitch_kd={:.2f}'.format(pkd))
-            self._cf.param.add_update_callback(group='pid_attitude',
-                                               name='pitch_kd',
-                                               cb=self._a_pitch_kd_callback)
-            # When setting a value the parameter is automatically read back
-            # and the registered callbacks will get the updated value
-            self._cf.param.set_value('pid_attitude.pitch_kd',
-                                     '{:.2f}'.format(pkd))
+                self._cf.param.remove_update_callback(group=g, cb=self._param_callback)
 
     def _a_pitch_kd_callback(self, name, value):
         """Callback for pid_attitude.pitch_kd"""
-        print('Readback: {0}={1}'.format(name, value))
+        print('Read back: {0}={1}'.format(name, value))
 
-        # End the example by closing the link (will cause the app to quit)
+        # This is the end of the example, close link
         self._cf.close_link()
 
     def _connection_failed(self, link_uri, msg):
@@ -150,21 +150,14 @@ class ParamExample:
 
 
 if __name__ == '__main__':
+    import logging
+    logging.getLogger().setLevel(logging.INFO)
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
-    # Scan for Crazyflies and use the first one found
-    print('Scanning interfaces for Crazyflies...')
-    available = cflib.crtp.scan_interfaces(address)
-    print('Crazyflies found:')
-    for i in available:
-        print(i[0])
+    pe = ParamExample(address)
 
-    if len(available) > 0:
-        pe = ParamExample(available[0][0])
-        # The Crazyflie lib doesn't contain anything to keep the application
-        # alive, so this is where your application should do something. In our
-        # case we are just waiting until we are disconnected.
-        while pe.is_connected:
-            time.sleep(1)
-    else:
-        print('No Crazyflies found, cannot run example')
+    # The Crazyflie lib doesn't contain anything to keep the application
+    # alive, so this is where your application should do something. In our
+    # case we are just waiting until we are disconnected.
+    while pe.is_connected:
+        time.sleep(1)

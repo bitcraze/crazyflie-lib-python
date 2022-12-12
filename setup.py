e5d2cd0eb407365f09f4197a5ecfe1d0df428468
Arnaud Taffanel <arnaud@bitcraze.io>
2022-12-12 13:41:02 +0100

    Update version to 0.1.21

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (directory / 'README.md').read_text()
 
 setup(
     name='cflib',
-    version='0.1.20.1',
+    version='0.1.21',
     packages=find_packages(exclude=['examples', 'test']),
 
     description='Crazyflie python driver',

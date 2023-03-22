28b7b7b902e6138ceff8efda8cc4a7b0fa994152
knmcguire <kimberly@bitcraze.io>
2023-03-22 17:41:01 +0100

    added cflib multiranger follower script

---
+++ examples/multiranger/multiranger_wall_following.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#     ||          ____  _ __
+#  +------+      / __ )(_) /_______________ _____  ___
+#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#
+#  Copyright (C) 2017 Bitcraze AB
+#
+#  Crazyflie Python Library
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+Example scripts that allows a user to "push" the Crazyflie 2.0 around
+using your hands while it's hovering.
+
+This examples uses the Flow and Multi-ranger decks to measure distances
+in all directions and tries to keep away from anything that comes closer
+than 0.2m by setting a velocity in the opposite direction.
+
+The demo is ended by either pressing Ctrl-C or by holding your hand above the
+Crazyflie.
+
+For the example to run the following hardware is needed:
+ * Crazyflie 2.0
+ * Crazyradio PA
+ * Flow deck
+ * Multiranger deck
+"""
+import logging
+import sys
+import time
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils import uri_helper
+from cflib.utils.multiranger import Multiranger
+
+from wall_following import WallFollowingMultiranger
+
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+
+if len(sys.argv) > 1:
+    URI = sys.argv[1]
+
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+
+wall_following = WallFollowingMultiranger(angle_value_buffer= 0.01, ref_distance_from_wall = 0.5, max_forward_speed = 0.3, init_state = WallFollowingMultiranger.StateWF.FORWARD)
+
+
+if __name__ == '__main__':
+    # Initialize the low-level drivers
+    cflib.crtp.init_drivers()
+
+    cf = Crazyflie(rw_cache='./cache')
+    with SyncCrazyflie(URI, cf=cf) as scf:
+        with MotionCommander(scf) as motion_commander:
+            with Multiranger(scf) as multiranger:
+                keep_flying = True
+
+                while keep_flying:
+                    VELOCITY = 0.5
+                    velocity_x = 0.0
+                    velocity_y = 0.0
+                    yaw_rate = 0.0
+
+
+                    #need yaw
+                    actual_yaw = 0.0;#motion_commander._yaw_estimator.get_yaw()
+
+                    velocity_x, velocity_y, yaw_rate = wall_following.wall_follower(multiranger.front, multiranger.right,actual_yaw,1,time.time())
+
+                    motion_commander.start_linear_motion(
+                        velocity_x, velocity_y, yaw_rate)
+
+                    time.sleep(0.1)
+
+            print('Demo terminated!')

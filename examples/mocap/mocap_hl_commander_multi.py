5095a9b0c596486487b0f2690d946faf066b6fbf
knmcguire <kimberly@bitcraze.io>
2023-12-18 13:50:32 +0100

    made a mocap script for 2 crazyflies

---
+++ examples/mocap/mocap_hl_commander_multi.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+#
+# ,---------,       ____  _ __
+# |  ,-^-,  |      / __ )(_) /_______________ _____  ___
+# | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+# | / ,--'  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#    +------`   /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#
+# Copyright (C) 2023 Bitcraze AB
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, in version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+Example of how to connect to a motion capture system and feed the position to a
+Crazyflie, using the motioncapture library. The motioncapture library supports all major mocap systems and provides
+a generalized API regardless of system type.
+The script uses the high level commander to upload a trajectory to fly a figure 8.
+
+Set the uri to the radio settings of the Crazyflie and modify the
+mocap setting matching your system.
+"""
+import time
+from threading import Thread
+
+import motioncapture
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.mem import MemoryElement
+from cflib.crazyflie.mem import Poly4D
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.syncLogger import SyncLogger
+from cflib.utils import uri_helper
+
+
+# The host name or ip address of the mocap system
+host_name = '192.168.5.21'
+
+# The type of the mocap system
+# Valid options are: 'vicon', 'optitrack', 'optitrack_closed_source', 'qualisys', 'nokov', 'vrpn', 'motionanalysis'
+mocap_system_type = 'qualisys'
+
+# True: send position and orientation; False: send position only
+send_full_pose = True
+
+
+class MocapWrapper(Thread):
+    def __init__(self, body_name):
+        Thread.__init__(self)
+
+        self.body_name = body_name
+        self.on_pose = None
+        self._stay_open = True
+
+        self.start()
+
+    def close(self):
+        self._stay_open = False
+
+    def run(self):
+        mc = motioncapture.connect(mocap_system_type, {'hostname': host_name})
+        while self._stay_open:
+            mc.waitForNextFrame()
+            for name, obj in mc.rigidBodies.items():
+                if name == self.body_name:
+                    if self.on_pose:
+                        pos = obj.position
+                        self.on_pose([pos[0], pos[1], pos[2], obj.rotation])
+
+
+URI1 = 'radio://0/30/2M/E7E7E7E711'
+URI2 = 'radio://0/30/2M/E7E7E7E712'
+
+uris = {
+    URI1,
+    URI2,
+    # Add more URIs if you want more copters in the swarm
+}
+
+seq_arg: {
+    URI1: MocapWrapper('cf1'),
+    URI2: MocapWrapper('cf2'),
+}
+
+
+def activate_kalman_estimator(scf):
+    scf.cf.param.set_value('stabilizer.estimator', '2')
+
+    # Set the std deviation for the quaternion data pushed into the
+    # kalman filter. The default value seems to be a bit too low.
+    scf.cf.param.set_value('locSrv.extQuatStdDev', 0.06)
+
+
+def send_extpose_quat(cf, x, y, z, quat):
+    """
+    Send the current Crazyflie X, Y, Z position and attitude as a quaternion.
+    This is going to be forwarded to the Crazyflie's position estimator.
+    """
+    if send_full_pose:
+        cf.extpos.send_extpose(x, y, z, quat.x, quat.y, quat.z, quat.w)
+    else:
+        cf.extpos.send_extpos(x, y, z)
+
+
+def init_extpose_quat(scf, mocap_wrapper):
+    mocap_wrapper.on_pose = lambda pose: send_extpose_quat(scf.cf, pose[0], pose[1], pose[2], pose[3])
+
+
+def run_shared_sequence(scf):
+    box_size = 1
+    flight_time = 2
+
+    commander = scf.cf.high_level_commander
+
+    commander.takeoff(1.0, 2.0)
+    time.sleep(3)
+
+    commander.go_to(box_size, 0, 0, 0, flight_time, relative=True)
+    time.sleep(flight_time)
+
+    commander.go_to(0, box_size, 0, 0, flight_time, relative=True)
+    time.sleep(flight_time)
+
+    commander.go_to(-box_size, 0, 0, 0, flight_time, relative=True)
+    time.sleep(flight_time)
+
+    commander.go_to(0, -box_size, 0, 0, flight_time, relative=True)
+    time.sleep(flight_time)
+
+    commander.land(0.0, 2.0)
+    time.sleep(2)
+
+    commander.stop()
+
+
+if __name__ == '__main__':
+    cflib.crtp.init_drivers()
+    factory = CachedCfFactory(rw_cache='./cache')
+
+    with Swarm(uris, factory=factory) as swarm:
+        swarm.parallel_safe(init_extpose_quat, args_dict=seq_arg)
+
+        swarm.parallel_safe(activate_kalman_estimator)
+        swarm.reset_estimators()
+
+        swarm.parallel_safe(run_shared_sequence)

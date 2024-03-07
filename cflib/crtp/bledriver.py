dd559aa8e19aab9c84170a0c00b99c658d6784af
UnexDev <26cgould2@gmail.com>
2024-03-07 11:01:13 -0500

    Partial connect logic and async disclaimer.

--- cflib/crtp/bledriver.py
+++ cflib/crtp/bledriver.py
@@ -2,19 +2,45 @@
 CRTP BLE driver.
 """
 
+import platform
+import re
+import asyncio
+
+from bleak import BleakClient as BLEClient, BleakScanner as BLEScanner
+
 from .crtpdriver import CRTPDriver
 from .crtpstack import CRTPPacket
-
+from .exceptions import WrongUriType
 __author__ = 'UnexDev'
 __all__ = ['CRTPDriver']
 
 
 class BLEDriver:
-    def __init__(self):
-        pass
+    """
+    Driver to interface with a CRTP-capable device over Bluetooth Low Energy (BLE).
+    """
+    address: str
+    client: BLEClient
+    def __init__(self, loop: asyncio.AbstractEventLoop):
+        self.address = ''
 
-    def connect(self, uri: str, link_quality_callback, link_error_callback):
-        pass
+    async def connect(self, uri: str, link_quality_callback, link_error_callback):
+        if not uri.startswith('ble://'):
+            raise WrongUriType('Not a BLE URI')
+        
+        address = uri.removeprefix('ble://')
+
+        is_valid_address = False
+        if platform.platform() == 'Darwin' and re.fullmatch(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}', address):
+                is_valid_address = True
+        else:
+            if re.fullmatch(r'([0-9A-F]{2}):([0-9A-F]{2}):([0-9A-F]{2}):([0-9A-F]{2}):([0-9A-F]{2}):([0-9A-F]{2})'):
+                is_valid_address = True
+        
+        if not is_valid_address:
+            raise WrongUriType('Not a BLE address')
+        
+        self.address = address
 
     def send_packet(self, pk: CRTPPacket):
         pass

f1ec5cc456f16e7ad80090516445dedbb2d1eb7b
Kristoffer Richardsson <kristoffer@bitcraze.io>
2025-05-16 13:38:31 +0200

    Added functions for live sample matching

--- cflib/localization/lighthouse_sample_matcher.py
+++ cflib/localization/lighthouse_sample_matcher.py
@@ -34,32 +34,75 @@ class LighthouseSampleMatcher:
     a list of LhCfPoseSample. Matching is done using the timestamp and a maximum time span.
     """
 
+    def __init__(self, max_time_diff: float = 0.020, min_nr_of_bs_in_match: int = 1) -> None:
+        self.max_time_diff = max_time_diff
+        self.min_nr_of_bs_in_match = min_nr_of_bs_in_match
+
+        self._current_angles: dict[int, LighthouseBsVectors] = {}
+        self._current_ts = 0.0
+
+    def match_one(self, sample: LhMeasurement) -> LhCfPoseSample | None:
+        """Aggregate samples close in time.
+        This function is used to match samples from multiple base stations into a single LhCfPoseSample.
+        The function will return None if the number of base stations in the sample is less than
+        the minimum number of base stations required for a match.
+        Note that a pose sample is returned upon the next call to this function, that is when the maximum time diff of
+        the first sample in the group has been exceeded.
+
+        Args:
+            sample (LhMeasurement): angles from one base station
+
+        Returns:
+            LhCfPoseSample | None: a pose sample if available, otherwise None
+        """
+        result = None
+        if len(self._current_angles) > 0:
+            if sample.timestamp > (self._current_ts + self.max_time_diff):
+                if len(self._current_angles) >= self.min_nr_of_bs_in_match:
+                    result = LhCfPoseSample(self._current_angles, timestamp=self._current_ts)
+
+                self._current_angles = {}
+
+        if len(self._current_angles) == 0:
+            self._current_ts = sample.timestamp
+
+        self._current_angles[sample.base_station_id] = sample.angles
+
+        return result
+
+    def purge(self) -> LhCfPoseSample | None:
+        """Purge the current angles and return a pose sample if available.
+
+        Returns:
+            LhCfPoseSample | None: a pose sample if available, otherwise None
+        """
+        result = None
+
+        if len(self._current_angles) >= self.min_nr_of_bs_in_match:
+            result = LhCfPoseSample(self._current_angles, timestamp=self._current_ts)
+
+        self._current_angles = {}
+        self._current_ts = 0.0
+
+        return result
+
     @classmethod
     def match(cls, samples: list[LhMeasurement], max_time_diff: float = 0.020,
               min_nr_of_bs_in_match: int = 1) -> list[LhCfPoseSample]:
         """
-        Aggregate samples close in time into lists
+        Aggregate samples in a list
         """
 
         result = []
-        current_angles: dict[int, LighthouseBsVectors] = {}
-        current_ts = 0.0
+        matcher = cls(max_time_diff, min_nr_of_bs_in_match)
 
         for sample in samples:
-            if len(current_angles) > 0:
-                if sample.timestamp > (current_ts + max_time_diff):
-                    if len(current_angles) >= min_nr_of_bs_in_match:
-                        pose_sample = LhCfPoseSample(current_angles, timestamp=current_ts)
-                        result.append(pose_sample)
+            pose_sample = matcher.match_one(sample)
+            if pose_sample is not None:
+                result.append(pose_sample)
 
-                    current_angles = {}
-
-            if len(current_angles) == 0:
-                current_ts = sample.timestamp
-            current_angles[sample.base_station_id] = sample.angles
-
-        if len(current_angles) >= min_nr_of_bs_in_match:
-            pose_sample = LhCfPoseSample(current_angles, timestamp=current_ts)
+        pose_sample = matcher.purge()
+        if pose_sample is not None:
             result.append(pose_sample)
 
         return result

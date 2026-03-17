4972fdfab75c507b18e89ba980778c00b0de85b2
Stefan Thorstenson <stefan@bitcraze.se>
2026-03-17 11:22:21 +0100

    Remove definition of mc variable, as it is unused

--- examples/step-by-step/sbs_motion_commander.py
+++ examples/step-by-step/sbs_motion_commander.py
@@ -84,7 +84,7 @@ def move_linear_simple(scf):
 
 
 def take_off_simple(scf):
-    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
+    with MotionCommander(scf, default_height=DEFAULT_HEIGHT):
         time.sleep(3)
 
 

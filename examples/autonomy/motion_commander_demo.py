5e80777e7667a68a0e90621284f5d783889a63c6
ArisMorgens <arismorsinkpaloumpas@gmail.com>
2026-08-19 14:57:44 +0200

    Updated copyright references

--- examples/autonomy/motion_commander_demo.py
+++ examples/autonomy/motion_commander_demo.py
@@ -6,7 +6,7 @@
 #  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
 #   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
 #
-#  Copyright (C) 2017 Bitcraze AB
+#  Copyright (C) 2017-2026 Bitcraze AB
 #
 #  Crazyflie Nano Quadcopter Client
 #
--- test/utils/test_callbacks.py
+++ test/utils/test_callbacks.py
@@ -6,7 +6,7 @@
 #  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
 #   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
 #
-#  Copyright (C) Bitcraze AB
+#  Copyright (C) 2016-2026 Bitcraze AB
 #
 #  Crazyflie Nano Quadcopter Client
 #
